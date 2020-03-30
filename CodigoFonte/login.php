daa74072888e56b1ece9fe9be1f39c2b69ed84af
Henrique <Henrique>
2020-03-29 21:53:34 -0300

    Apagar o readme.md

--- CodigoFonte/login.php
+++ CodigoFonte/login.php
@@ -1,19 +1,7 @@
 <?php 
-    require_once "ModeloDAO/conexao.php";
-    require_once "Modelo"
-
-    class login {
-        
-        private $conexao;
-            #preciso primeiro do crud do usuario
-        public function login(){}
 
 
-
-
-    }
-
-
+    
 
 
 
--- README.md
+++ README.md
@@ -1,26 +1,4 @@
-"Documentação do PI
-    #
-    Esse programa foi feito em CRUD
-    
-    Controlador{ 
-        
-
-
-
-
-
-    }
-
-    Modelo{}
-
-    ModeloDAO{}
-
-    View{}
-
-
-
-
-
+"Documentação do PI"
 
 
 
